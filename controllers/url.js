61b37e3568fa471d74419ba6a7760aae1c66fc84
Pranshu Sachan <pranshusachan65@gmail.com>
2024-10-11 23:07:19 +0530

    crash handling and status code

--- controllers/url.js
+++ controllers/url.js
@@ -1,36 +1,45 @@
+const { json } = require('express');
 const urlModel = require('../models/url')
 const shortid = require('shortid');
 
 async function shortUrlGenerator(req, res) {
     const body = req.body;
     console.log("body is ", body)
-    if(! body.url) return res.status(400).json({"err" : " in-valid body"})
+    if (!body.url) return res.status(400).json({ "err": " in-valid body" })
     //const shortId = shortid.generate()
     const shortId = shortid()
     await urlModel.create({
-        shortId : shortId,
-        longId : body.url,
-        totatClicks : 1
+        shortId: shortId,
+        longId: body.url,
+        totatClicks: 1
     })
 
-    return res.json({ id : shortId})
+    res.status(200).json({ id: shortId })
 }
 
 async function visitUrl(req, res) {
     const shortId = req.params.shortid
     console.log("short id is ", shortId)
-   const entry = await urlModel.findOneAndUpdate({
+    const entry = await urlModel.findOneAndUpdate({
         shortId
-    }, { $inc : {totatClicks : 1}})
+    }, { $inc: { totatClicks: 1 } })
     console.log("entry is ", entry)
-    res.redirect(entry.longId)
+    if (entry && entry.longId) {
+        res.redirect(entry.longId)
+    }
+    else {
+        res.status(500).json({ "err": "internal server error" })
+    }
 }
 
 async function ClickCount(req, res) {
     const shortId = req.params.shortid
-    const entry = await urlModel.findOne({shortId})
+    const entry = await urlModel.findOne({ shortId })
     console.log("entry is ", entry)
-    res.status(200).json({"click count is ": entry.totatClicks})
+    if (entry)
+        res.status(200).json({ "click count is ": entry.totatClicks })
+    else
+        res.status(500).json({ "err": "internal server error" })
 }
 
-module.exports = {shortUrlGenerator, visitUrl, ClickCount}
+module.exports = { shortUrlGenerator, visitUrl, ClickCount }
